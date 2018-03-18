b67053fd0bf20356c2a694d1fef9d0b8850f9e4a
Ramesh_G <37336675+rameshnickms@users.noreply.github.com>
2018-03-18 16:20:59 +0530

    Create Given day Holiday or not.py

---
+++ Player/Set2/Given day Holiday or not.py
@@ -0,0 +1,9 @@
+i=input()
+
+if(i=="Sunday" or i=="Saturday"):
+  print("yes")
+elif(i=="Monday" or i=="Tuesday" or i=="Wednesday" or i=="Thursday" or i=="Friday"):
+  print("no")
+
+else:
+  print("Invaid input")
