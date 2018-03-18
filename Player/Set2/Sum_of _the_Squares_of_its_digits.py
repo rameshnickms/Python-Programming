782d2c089a41781635aedff541bd7460a38955cc
Ramesh_G <37336675+rameshnickms@users.noreply.github.com>
2018-03-18 19:51:20 +0530

    Update Sum_of _the_Squares_of_its_digits.py

--- Player/Set2/Sum_of _the_Squares_of_its_digits.py
+++ Player/Set2/Sum_of _the_Squares_of_its_digits.py
@@ -4,13 +4,11 @@ except:
   print("Invalid Input")
 else:
   sum=0
-  if(a>=1 and a<=1000000000000000000):
-    while(a>0):
-      b=(a%10)
-      c=(int(a/10))
-      sum+=b**2
-      a=c
-    print(sum)
+  while(a>0):
+    b=(a%10)
+    c=(int(a/10))
+    sum+=b**2
+    a=c
+  print(sum)
   
-  else:
-    print("Invalid Input")
+  
