f9d162dc98987923655f2936a0e9ab9eaf5539fd
Ramesh_G <37336675+rameshnickms@users.noreply.github.com>
2018-03-18 11:49:44 +0530

    Update Reversing a String.py

--- Player/Reversing a String.py
+++ Player/Reversing a String.py
@@ -1,6 +1,15 @@
+
 a=input()
-b=len(a)
-if(b<=6):
-  print(a[::-1])
+
+try:
+  c=int(a)
+except:
+    
+  b=len(a)
+  if(b<=6):
+    print(a[::-1])
+  else:
+    print("Invalid Input")
+
 else:
   print("Invalid Input")
