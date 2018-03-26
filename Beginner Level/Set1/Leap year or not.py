7de1fba08bf7827d21d8cdfc52be710133142d7d
Ramesh_G <37336675+rameshnickms@users.noreply.github.com>
2018-03-26 22:00:55 +0530

    Create Leap year or not.py

---
+++ Beginner Level/Set1/Leap year or not.py
@@ -0,0 +1,15 @@
+try:
+  a=input()
+  year=int(a)
+except:
+  print("Invalid Input")
+else:
+  
+  if ( year%400 == 0):
+    print("yes")
+  elif ( year%100 == 0):
+    print("no")
+  elif ( year%4 == 0 ): 
+    print("yes")
+  else :
+    print("no") 
