a77dee0c2511dbc03338b8b6cfdaac16eb893962
Ramesh_G <37336675+rameshnickms@users.noreply.github.com>
2018-03-28 21:28:31 +0530

    Update print the odd no's in the inputs given.py

--- Beginner Level/Set2/print the odd no's in the inputs given.py
+++ Beginner Level/Set2/print the odd no's in the inputs given.py
@@ -1,12 +1,16 @@
+x=list()
 a,b=input().split()
-p=int(a)
-q=int(b)
-c=max(p,q)
-d=min(p,q)
-for i in range (d+1,c):
-  if(i%2 == 0):
-    #print(i)
-    continue
-  else:
-    print(i)
-   
+try:
+  p=int(a)
+  q=int(b)
+except:
+  print("Invalid Input")
+else:
+  c=max(p,q)
+  d=min(p,q)
+  for i in range (d+1,c):
+    if(i%2 == 0):
+      continue
+    else:
+      x.append(str(i))
+  print(" ".join(x))
